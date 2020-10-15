8484f4b0b5a07f2a29e85cc47461397b31d1358a
Julien Flusin <julienflusin3@gmail.com>
2020-10-15 21:47:41 +0200

    More flexibility and added "0" if number is below 10

--- change-hostname.ps1
+++ change-hostname.ps1
@@ -1,6 +1,14 @@
+$numerosalle = Get-Content -Path .\numerosalle.txt
 $valuestr = Get-Content -Path .\counter.txt
 $valueint = ($valuestr -as [int])
-Rename-Computer -NewName "PC-233-$valueint"
+if($valueint -le 9){
+    $valuestr2 = "0" + $valueint
+}
+else{
+    $valuestr2 = $valueint
+    echo $valuestr2
+}
+Rename-Computer -NewName "PC-$numerosalle-$valuestr2"
 $valueint++
 Set-Content -Path .\counter.txt -Value $valueint
 Restart-Computer
